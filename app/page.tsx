f5b875506476448ef41bedb104184dfae91858b6
Mohamed mabrouk <mohamed.mabrouk@flexperto.com>
2024-08-14 10:40:16 +0200

    first page & layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,125 @@
+import FramerWrapper from "@/components/framer-wrapper";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import {
+  Download,
+  ExternalLink,
+  Facebook,
+  Github,
+  Linkedin,
+  Twitter,
+} from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+
+const SocialLinks = () => {
+  const links = [
+    {
+      name: "Facebook",
+      link: "https://www.facebook.com/mednacer.mabrouk",
+      icon: <Facebook />,
+    },
+    {
+      name: "Twitter",
+      link: "https://x.com/med_naceur89",
+      icon: <Twitter />,
+    },
+    {
+      name: "Linkedin",
+      link: "https://www.linkedin.com/in/mohamed-naceur-mabrouk-727714124",
+      icon: <Linkedin />,
+    },
+    {
+      name: "External",
+      link: "https://www.xing.com/profile/MohamedNaceur_Mabrouk",
+      icon: <ExternalLink />,
+    },
+  ];
+  return (
+    <>
+      {links.map((itm, indx) => {
+        const timing = 0.55 + indx * 0.125;
+
+        return (
+          <FramerWrapper key={indx} delay={timing} y={50}>
+            <Link
+              target="blank"
+              href={itm.link}
+              className={cn(
+                buttonVariants({ variant: "outline", size: "icon" })
+              )}
+            >
+              {itm.icon}
+            </Link>
+          </FramerWrapper>
+        );
+      })}
+    </>
+  );
+};
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <FramerWrapper
+        className=" h-full w-auto flex flex-col justify-start gap-4"
+        y={0}
+        x={-100}
+      >
+        <h3 className="font-poppins text-2xl max-sm:text-xl">My Name is</h3>
+        <h1 className="font-rubik text-8xl name_underline text-primary max-sm:text-6xl ">
+          Mohamed Naceur <br /> Mabrouk .
+        </h1>
+        <div className="py-4  rounded-md flex flex-col justify-center items-center overflow-hidden">
+          <div className="font-poppins text-base sm:text-2xl [text-wrap:balance] text-gray-700">
+            I am a Software Engineer &
+            <span className="inline-flex ml-2 flex-col h-[calc(theme(fontSize.lg)*theme(lineHeight.tight))] sm:h-[calc(theme(fontSize.3xl)*theme(lineHeight.tight))] overflow-hidden">
+              <ul className="block text-left font-rubik text-lg sm:text-3xl leading-tight [&_li]:block animate-text-slide">
+                <li className="text-[#2f7df4]">Freelancer</li>
+                <li className="text-[#2f7df4]">Blogger</li>
+                <li className="text-[#2f7df4]">Gamer</li>
+                <li className="text-[#2f7df4]">Creator</li>
+                <li className="text-[#2f7df4]">Developer</li>
+                <li className="text-[#2f7df4]">Contributor</li>
+              </ul>
+            </span>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+        <div className="h-fit w-full p-4 flex gap-3">
+          <SocialLinks />
+        </div>
+        <div className="h-fit w-full mt-2 py-2 px-4">
+          <Button size={"lg"} className="text-base px-5 py-6">
+            <Download className="mx-1" />
+            Download Resume
+          </Button>
+        </div>
+      </FramerWrapper>
+      {/* RIGHT SIDE image  */}
+      <FramerWrapper
+        className="h-full w-[47%] relative block max-lg:hidden"
+        y={0}
+        x={100}
+      >
+        {/* IMAGE  */}
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          src="/logo.png"
+          alt="logo"
+          loading="eager"
           priority
+          height={1000}
+          width={1000}
         />
-      </div>
+      </FramerWrapper>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <Link
+        href={"https://github.com/mnm89"}
+        target="blank"
+        className=" animate-pulse absolute left-0 bottom-5 flex rounded-r-full justify-center items-center gap-2 z-50 w-fit h-fit p-2 shadow-md  border-y border-r  border-black hover:bg-primary hover:text-white hover:animate-none "
+      >
+        <Github />
+        <span className="font-rubik text-2xl max-sm:text-xl">Github</span>
+      </Link>
+    </>
   );
 }
