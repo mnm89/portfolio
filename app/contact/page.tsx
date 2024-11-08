55eb71f678b68662378ee0928b910fa76d77ed98
Mohamed mabrouk <mohamed.mabrouk@flexperto.com>
2024-11-08 13:15:25 +0100

    update contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,6 +6,18 @@ import { Mail, Phone } from "lucide-react";
 export default function ContactPage() {
   return (
     <section className="flex flex-col gap-2 justify-center items-center w-full h-[80vh]">
+      <p className="text-xl text-pretty mx-auto">
+        Starting a career in tech as a software engineer is an exciting choice
+        for me because it allows me to channel my passion for problem-solving
+        into creating impactful, innovative solutions. Technology is a dynamic
+        field with endless opportunities for growth and learning, and it’s
+        rewarding to be part of an industry that shapes how we interact, work,
+        and live. I’m motivated by the chance to build meaningful, efficient
+        applications that address real-world needs and to continuously improve
+        my skills in a field that’s always evolving. Tech offers a creative
+        outlet that challenges me intellectually and aligns with my ambition to
+        make a positive impact through software.
+      </p>
       <h1 className="text-4xl text-pretty mx-auto text-secondary-foreground">
         You want to contact me ?
       </h1>
